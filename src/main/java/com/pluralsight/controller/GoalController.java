a1e9c5a0c47f09e69c02e07eea9bce1b1333f8b5
Joshua Barnett <joecoolatbyu@ldschurch.org>
2018-08-09 21:43:59 -0600

    Added Goal Service Class

--- src/main/java/com/pluralsight/controller/GoalController.java
+++ src/main/java/com/pluralsight/controller/GoalController.java
@@ -1,7 +1,8 @@
 package com.pluralsight.controller;
 
-import javax.validation.Valid;
-
+import com.pluralsight.model.Goal;
+import com.pluralsight.service.GoalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.pluralsight.model.Goal;
+import javax.validation.Valid;
 
 @Controller
 @SessionAttributes("goal")
 public class GoalController {
+
+	@Autowired
+	private GoalService goalService;
 
 	@RequestMapping(value = "addGoal", method = RequestMethod.GET)
 	public String addGoal(Model model) {
@@ -34,6 +38,8 @@ public class GoalController {
 		
 		if(result.hasErrors()) {
 			return "addGoal";
+		} else {
+			goalService.save(goal);
 		}
 		
 		return "redirect:index.jsp";
